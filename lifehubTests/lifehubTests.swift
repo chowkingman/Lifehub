3a35cd3c58754f6bd6a2318e1470febc1eae4903
King-Man Chow <kingmanchowswe@gmail.com>
2025-02-02 16:17:37 -0600

    Initial Commit

---
+++ lifehubTests/lifehubTests.swift
@@ -0,0 +1,17 @@
+//
+//  lifehubTests.swift
+//  lifehubTests
+//
+//  Created by King-Man Chow on 2/2/25.
+//
+
+import Testing
+@testable import lifehub
+
+struct lifehubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
